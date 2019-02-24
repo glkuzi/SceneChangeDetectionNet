Fixed nan losses, fixed normalization

model.py
@@ -5,12 +5,12 @@ Created on Sat Feb 23 23:47:44 2019
 @author: User
 """
 
-
+import tensorflow as tf
 import numpy as np
 import keras.backend as K
 from tensorflow.math import reduce_max
 from generator import *
-from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, Lambda, UpSampling2D
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, Lambda, UpSampling2D, BatchNormalization
 from keras.models import Sequential, Model
 from keras import regularizers
 from keras import optimizers
@@ -30,6 +30,12 @@ def toTarget(tensor):
     #return imresize(np.array(tensor, dtype='float') / float(K.max(K.flatten(tensor))), 8.0)
 
 
+def normal(tensors):
+    tensor = K.sqrt(K.square(tensors[0]) - K.square(tensors[1]))
+    tensor = tf.div(tf.subtract(tensor, tf.reduce_min(tensor)), tf.subtract(tf.reduce_max(tensor), tf.reduce_min(tensor)))
+    return tensor
+
+
 def get_siamese_model(input_shape):
     """
         Model architecture
@@ -41,27 +47,32 @@ def get_siamese_model(input_shape):
     
     # Convolutional Neural Network
     model = Sequential()
-    model.add(Conv2D(16, (10, 10), activation='relu', input_shape=input_shape, #64
+    model.add(Conv2D(16, (10, 10), activation='sigmoid', input_shape=input_shape, #64
                      kernel_regularizer=regularizers.l2(2e-4)))
     model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Conv2D(8, (7, 7), activation='relu', #128
+    model.add(Conv2D(8, (7, 7), #128
                      kernel_regularizer=regularizers.l2(2e-4)))
     model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Conv2D(4, (4, 4), activation='relu', #128
+    model.add(Conv2D(4, (4, 4), #128
                      kernel_regularizer=regularizers.l2(2e-4)))
     model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Conv2D(3, (2, 2), activation='relu',
+    model.add(Conv2D(3, (2, 2),
                      kernel_regularizer=regularizers.l2(2e-4)))
     #model.add(Conv2D(3, (4, 4), activation='relu',
     #                 kernel_regularizer=regularizers.l2(2e-4)))
     # Generate the encodings (feature vectors) for the two images
     encoded_l = model(left_input)
     encoded_r = model(right_input)
-    
     # Add a customized layer to compute the absolute difference between the encodings
-    L2_layer = Lambda(lambda tensors:K.sqrt(K.square(tensors[0]) - K.square(tensors[1])) / norm(K.sqrt(K.square(tensors[0]) - K.square(tensors[1])), ord=1))
+    #L2_layer = Lambda(lambda tensors:(normal(tensors)))#K.sqrt(K.square(tensors[0]) - K.square(tensors[1])) / norm(K.sqrt(K.square(tensors[0]) - K.square(tensors[1])), ord=1))
+    #L2_layer = Lambda(normal, arguments=tensors)
+    #L2_layer = Lambda(lambda tensors:K.normalize_batch_in_training( K.sqrt(K.square(tensors[0]) - K.square(tensors[1])), 
+    #                                                               K.ones_like(K.sqrt(K.square(tensors[0]) - K.square(tensors[1]))), 
+    #                                                               K.zeros_like(K.sqrt(K.square(tensors[0]) - K.square(tensors[1]))) ))
+    L2_layer = Lambda(lambda tensors:(K.sqrt(K.square(tensors[0]) - K.square(tensors[1]))))
     L2_distance = L2_layer([encoded_l, encoded_r])
     UpLayer = UpSampling2D(size=(8, 8))(L2_distance)
+    NormLayer = BatchNormalization()(UpLayer)
     #UpLayer = Lambda(lambda tensor:toTarget(tensor))(L2_distance)
     # Add a dense layer with a sigmoid unit to generate the similarity score
     #prediction = Dense(1,activation='sigmoid')(L2_distance)
@@ -80,7 +91,7 @@ def main():
     valid = DataGenerator(patches_val)
     model = get_siamese_model(inp_shape)
     check = ModelCheckpoint('testn.{epoch:02d}-{val_loss:.2f}.hdf5', period=1)
-    model.compile(loss='cosine_proximity', optimizer=optimizers.Adam(lr=0.001, decay=0.0001), metrics=['accuracy'])
+    model.compile(loss='mae', optimizer=optimizers.Adam(lr=0.0001, decay=0.0001), metrics=['accuracy'])
     history = model.fit_generator(generator=dg, steps_per_epoch=32, epochs=50, verbose=1, callbacks=[check],
                         validation_data=valid)
     plt.plot(history.history['loss'])

predict.py
@@ -13,17 +13,26 @@ from keras.models import load_model
 
 
 def main():
-    model_path = 'testn.01-nan.hdf5'
+    model_path = 'testn.01-0.05.hdf5'
     path1 = 'C:\Download\SevStal\TESTSET\IMG_20190223_140201.jpg'
     path2 = 'C:\Download\SevStal\TESTSET\IMG_20190223_140210.jpg'
+    tpath = '‪C:\Download\SevStal\dataset\PTZ\twoPositionPTZCam\gt_binary\gt001386.png'
     buf1 = preprocessing(Image.open(path1), np.array(Means[0]))
     buf2 = preprocessing(Image.open(path2), np.array(Means[1]))
+    targ = preprocessing(Image.open(path2), [0, 0, 0])
+    print(targ, np.max(targ))
     X = [np.array([buf1]), np.array([buf2])]
+    
     model = load_model(model_path)
     Y = model.predict(X)
-    print(Y)
+    print(Y, np.max(Y / np.max(Y)))
+    Y = Y /np.max(Y)
+    Y = np.array(Y * 255, dtype='uint8')[0]
+    print(Y, Y.shape)
     img = Image.fromarray(Y)
-    Image.SAVE('test.png', img)
+    print(img)
+    img.save('test.png')
+    
     return 0
 
 